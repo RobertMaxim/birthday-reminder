94c604dba74975587cdc5ad6ede3590b43a08d50
Robert Maxim <rmaxim@talentingsoftware.com>
2023-04-30 15:28:40 +0300

    added login and register page

--- src/app/login-module/login-module.module.ts
+++ src/app/login-module/login-module.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { NzCardModule } from 'ng-zorro-antd/card';
 import { LoginModuleRoutingModule } from './login-module-routing.module';
 import { LoginComponent } from './login/login.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { RegisterComponent } from './register/register/register.component';
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
     LoginModuleRoutingModule,
-    NzButtonModule
+    NzButtonModule,
+    NzFormModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzCardModule,
+    NzCheckboxModule
   ]
 })
 export class LoginModuleModule { }
--- src/app/login-module/login/login.component.ts
+++ src/app/login-module/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Validators,UntypedFormBuilder , UntypedFormGroup} from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  validateForm!: UntypedFormGroup;
 
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  constructor(private fb: UntypedFormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+    });
+  }
 }

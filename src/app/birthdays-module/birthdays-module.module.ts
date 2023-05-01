9d8ecb8d525680defe26c59f8644354d45585361
Andrei Beres <andrei.beres@yahoo.com>
2023-05-01 15:55:43 +0300

    Fixed styling issues

--- src/app/birthdays-module/birthdays-module.module.ts
+++ src/app/birthdays-module/birthdays-module.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { BirthdaysModuleRoutingModule } from './birthdays-module-routing.module';
 import { BirthdaysComponent } from './birthdays/birthdays.component';
 import { NzTableModule } from 'ng-zorro-antd/table';
@@ -8,6 +7,7 @@ import { TopbarComponent } from './topbar/topbar.component';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+
 @NgModule({
   declarations: [
     BirthdaysComponent,
